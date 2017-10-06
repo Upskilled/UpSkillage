51cf223aabea75cadcd0a5d3f1516a272e86929c
Skeksalot <Skeksalot@users.noreply.github.com>
2017-10-06 11:37:29 +1030

    Readme finished testing

--- js_css_lib.js
+++ js_css_lib.js
@@ -193,7 +193,7 @@ function pluralsight( container ) {
 	img.alt = " ";
 	span.appendChild(img);
 	span.appendChild( document.createTextNode(" Pluralsight eLearning") );
-	span.style = "font-weight: bold;";
+	span.style.fontWeight = "bold";
 	pluralFlip.appendChild(span);
 	span = document.createElement("span");
 	span.appendChild( document.createTextNode(" (show/hide)") );
@@ -250,7 +250,7 @@ function lynda( container ) {
 	img.alt = " ";
 	span.appendChild(img);
 	span.appendChild( document.createTextNode(" Lynda.com eLearning") );
-	span.style = "font-weight: bold;";
+	span.style.fontWeight = "bold";
 	lyndaFlip.appendChild(span);
 	span = document.createElement("span");
 	span.appendChild( document.createTextNode(" (show/hide)") );
@@ -322,7 +322,46 @@ function lynda( container ) {
 }
 
 function microsoft( container ) {
+	var microFlip = document.createElement("div");
+	microFlip.id = "microFlip";
+	microFlip.className = "flip";
+	var span = document.createElement("span");
+	var img = document.createElement("img");
+	img.src = "https://www.google.com/s2/favicons?domain=microsoft.com";
+	img.className = "panelIcon";
+	img.alt = " ";
+	span.appendChild(img);
+	span.appendChild( document.createTextNode(" Microsoft Virtual Academy") );
+	span.style.fontWeight = "bold";
+	microFlip.appendChild(span);
+	span = document.createElement("span");
+	span.appendChild( document.createTextNode(" (show/hide)") );
+	microFlip.appendChild(span);
 	
+	var microPan = document.createElement("div");
+	microPan.id = "microPan";
+	microPan.className= "panel";
+	var p = document.createElement("p");
+	p.appendChild( document.createTextNode("") );
+	microPan.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("") );
+	microPan.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("") );
+	microPan.appendChild(p);
+	
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("") );
+	microPan.appendChild(p);
+	
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("") );
+	microPan.appendChild(p);
+	
+	var readme = document.getElementById(container);
+	readme.appendChild(microFlip);
+	readme.appendChild(microPan);
 }
 
 function assessment( container ) {
@@ -331,7 +370,7 @@ function assessment( container ) {
 	assessmentFlip.className = "flip";
 	var span = document.createElement("span");
 	span.appendChild( document.createTextNode("Assessments Explained") );
-	span.style = "font-weight: bold;";
+	span.style.fontWeight = "bold";
 	assessmentFlip.appendChild(span);
 	span = document.createElement("span");
 	span.appendChild( document.createTextNode(" (show/hide)") );
@@ -489,7 +528,7 @@ function rubric( container ) {
 	rubricFlip.className = "flip";
 	var span = document.createElement("span");
 	span.appendChild( document.createTextNode("Assessment Rubrics File") );
-	span.style = "font-weight: bold;";
+	span.style.fontWeight = "bold";
 	rubricFlip.appendChild(span);
 	span = document.createElement("span");
 	span.appendChild( document.createTextNode(" (show/hide)") );
@@ -561,7 +600,7 @@ function grades( container ) {
 	gradesFlip.className = "flip";
 	var span = document.createElement("span");
 	span.appendChild( document.createTextNode("Grades") );
-	span.style = "font-weight: bold;";
+	span.style.fontWeight = "bold";
 	gradesFlip.appendChild(span);
 	span = document.createElement("span");
 	span.appendChild( document.createTextNode(" (show/hide)") );
@@ -612,6 +651,344 @@ function grades( container ) {
 	var readme = document.getElementById(container);
 	readme.appendChild(gradesFlip);
 	readme.appendChild(gradesPan);
+}
+
+function dev( container ) {
+	var devFlip = document.createElement("div");
+	devFlip.id = "devFlip";
+	devFlip.className = "flip";
+	var span = document.createElement("span");
+	span.appendChild( document.createTextNode("Foundations Course") );
+	span.style.fontWeight = "bold";
+	devFlip.appendChild(span);
+	span = document.createElement("span");
+	span.appendChild( document.createTextNode(" (show/hide)") );
+	devFlip.appendChild(span);
+	
+	var devPan = document.createElement("div");
+	devPan.id = "devPan";
+	devPan.className= "panel";
+	var p = document.createElement("p");
+	p.appendChild( document.createTextNode("Upskilled has created a Foundations course to support you.") );
+	devPan.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("This course is not part of your qualification course and will not be assessed in any way.") );
+	devPan.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("We have created this course with basic modules of learning for those of you who would like to get a refresher on some basics or to just get into the swing of IT.") );
+	devPan.appendChild(p);
+	var div = document.createElement("div");
+	var label = document.createElement("label");
+	label.htmlFor = "devCheck";
+	label.style.display = "inline";
+	label.innerHTML = "Check the box to gain access the foundations course before beginning your course proper.";
+	div.appendChild(label);
+	var input = document.createElement("input");
+	input.type = "checkbox";
+	input.id = "devCheck";
+	input.name = "devCheck"
+	input.onclick = function(){ showCourse("dev"); };
+	input.value = "off";
+	input.style.display = "inline";
+	div.appendChild(input);
+	devPan.appendChild(div);
+	div = document.createElement("div");
+	div.id = "devCourse";
+	div.style.display = "none";
+	var br = document.createElement("br");
+	div.appendChild(br);
+	div.appendChild(br);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("") );
+	div.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("Copy this key first - ") );
+	span = document.createElement("span");
+	span.appendChild( document.createTextNode("2014DevFun") );
+	span.style.fontWeight = "bold";
+	p.appendChild(span);
+	div.appendChild(p);
+	var a = document.createElement("a");
+	a.href = "https://lms.upskilled.edu.au/course/view.php?id=1176";
+	a.target = "_blank";
+	a.appendChild( document.createTextNode("Information Technology Development Foundations") );
+	a.style.fontWeight = "bold";
+	div.appendChild(a);
+	devPan.appendChild(div);
+	
+	var readme = document.getElementById(container);
+	readme.appendChild(devFlip);
+	readme.appendChild(devPan);
+}
+
+function itif( container ) {
+	var itifFlip = document.createElement("div");
+	itifFlip.id = "itifFlip";
+	itifFlip.className = "flip";
+	var span = document.createElement("span");
+	span.appendChild( document.createTextNode("Foundations Course") );
+	span.style.fontWeight = "bold";
+	itifFlip.appendChild(span);
+	span = document.createElement("span");
+	span.appendChild( document.createTextNode(" (show/hide)") );
+	itifFlip.appendChild(span);
+	
+	var itifPan = document.createElement("div");
+	itifPan.id = "itifPan";
+	itifPan.className= "panel";
+	var p = document.createElement("p");
+	p.appendChild( document.createTextNode("Upskilled has created a Foundations course to support you.") );
+	itifPan.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("This course is not part of your qualification course and will not be assessed in any way.") );
+	itifPan.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("We have created this course with basic modules of learning for those of you who would like to get a refresher on some basics or to just get into the swing of IT.") );
+	itifPan.appendChild(p);
+	var div = document.createElement("div");
+	var label = document.createElement("label");
+	label.htmlFor = "itifCheck";
+	label.style.display = "inline";
+	label.innerHTML = "Check the box to gain access the foundations course before beginning your course proper.";
+	div.appendChild(label);
+	var input = document.createElement("input");
+	input.type = "checkbox";
+	input.id = "itifCheck";
+	input.name = "itifCheck"
+	input.onclick = function(){ showCourse("itif"); };
+	input.value = "off";
+	input.style.display = "inline";
+	div.appendChild(input);
+	itifPan.appendChild(div);
+	div = document.createElement("div");
+	div.id = "itifCourse";
+	div.style.display = "none";
+	var br = document.createElement("br");
+	div.appendChild(br);
+	div.appendChild(br);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("") );
+	div.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("Copy this key first - ") );
+	span = document.createElement("span");
+	span.appendChild( document.createTextNode("2014ITIF") );
+	span.style.fontWeight = "bold";
+	p.appendChild(span);
+	div.appendChild(p);
+	var a = document.createElement("a");
+	a.href = "https://lms.upskilled.edu.au/course/view.php?id=1175";
+	a.target = "_blank";
+	a.appendChild( document.createTextNode("Information Technology Development Foundations") );
+	a.style.fontWeight = "bold";
+	div.appendChild(a);
+	itifPan.appendChild(div);
+	
+	var readme = document.getElementById(container);
+	readme.appendChild(itifFlip);
+	readme.appendChild(itifPan);
+}
+
+function workplace( container ) {
+	var workplaceFlip = document.createElement("div");
+	workplaceFlip.id = "workplaceFlip";
+	workplaceFlip.className = "flip";
+	var span = document.createElement("span");
+	span.appendChild( document.createTextNode("Workplace Activities instead of Set Assessments?") );
+	span.style.fontWeight = "bold";
+	workplaceFlip.appendChild(span);
+	span = document.createElement("span");
+	span.appendChild( document.createTextNode(" (show/hide)") );
+	workplaceFlip.appendChild(span);
+	
+	var workplacePan = document.createElement("div");
+	workplacePan.id = "workplacePan";
+	workplacePan.className= "panel";
+	var p = document.createElement("p");
+	p.appendChild( document.createTextNode("In this qualification course you will find many learning materials and assessments.") );
+	workplacePan.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("You may find that some of the assessments we set are similar to activities you do at work.") );
+	workplacePan.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("If you believe that one of your workplace activities would be appropriate to submit as an assessment instead of completing the assessment listed in the cluster, then you are welcome to apply to submit your work activity as the assessment task. Most assignments or projects will have a 'Marking Guide' which you can use as a guide to the suitability of your workplace activity as a substitute assessment.") );
+	workplacePan.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("This is not a requirement, just an option for you if you want to use it.") );
+	workplacePan.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("Download the 'Workplace Activity as Assessment' form, complete it and upload it for your teacher to approve.") );
+	workplacePan.appendChild(p);
+	var a = document.createElement("a");
+	a.href = "#";
+	a.onclick = function() { popupDoc( 'https://rebrand.ly/workAssess', 'workAssess' ); };
+	var img = document.createElement("img");
+	img.src = "https://lms.upskilled.edu.au/theme/image.php?theme=ups&component=core&rev=1447640343&image=f%2Fdocument-24";
+	img.className = "activityicon iconlarge";
+	img.alt = "docx";
+	a.appendChild(img);
+	a.appendChild( document.createTextNode("Workplace Activity as Assessment") );
+	a.style.fontWeight = "bold";
+	workplacePan.appendChild(a);
+	
+	var readme = document.getElementById(container);
+	readme.appendChild(workplaceFlip);
+	readme.appendChild(workplacePan);
+}
+
+function review( container ) {
+	var reviewFlip = document.createElement("div");
+	reviewFlip.id = "reviewFlip";
+	reviewFlip.className = "flip";
+	var span = document.createElement("span");
+	span.appendChild( document.createTextNode("Uploading Files for Teacher Review") );
+	span.style.fontWeight = "bold";
+	reviewFlip.appendChild(span);
+	span = document.createElement("span");
+	span.appendChild( document.createTextNode(" (show/hide)") );
+	reviewFlip.appendChild(span);
+	
+	var reviewPan = document.createElement("div");
+	reviewPan.id = "reviewPan";
+	reviewPan.className= "panel";
+	var p = document.createElement("p");
+	p.appendChild( document.createTextNode("If you would like to upload development code to your teacher, this is the place to do it.") );
+	reviewPan.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("Please 'zip' your build directory into one file and upload using the link below. The size of the file can be no larger than 10MB.") );
+	reviewPan.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("You can only upload one file at a time, hence combining your build directory into one compressed file. Once your teacher has reviewed your code, the link will be activated again to make it available for future use.") );
+	reviewPan.appendChild(p);
+	
+	var readme = document.getElementById(container);
+	readme.appendChild(reviewFlip);
+	readme.appendChild(reviewPan);
+}
+
+function iot( container ) {
+	var iotFlip = document.createElement("div");
+	iotFlip.id = "iotFlip";
+	iotFlip.className = "flip";
+	var span = document.createElement("span");
+	span.appendChild( document.createTextNode("Purchasing your Pi and kit") );
+	span.style.fontWeight = "bold";
+	iotFlip.appendChild(span);
+	span = document.createElement("span");
+	span.appendChild( document.createTextNode(" (show/hide)") );
+	iotFlip.appendChild(span);
+	
+	var iotPan = document.createElement("div");
+	iotPan.id = "iotPan";
+	iotPan.className= "panel";
+	var p = document.createElement("p");
+	p.appendChild( document.createTextNode("You have enrolled in the Certificate IV Information Technology - Internet of Things Specialisation.") );
+	iotPan.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("This course has hardware requirements that you need to purchase to use in the course.") );
+	iotPan.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("The details are as follows:") );
+	p.appendChild( document.createElement("br") );
+	p.appendChild( document.createTextNode("1 x Raspberry Pi 2 model B.  These are widely available through distributors in Australia or online stores, ensure it has an Australian plug on the power adapter included.") );
+	p.appendChild( document.createElement("br") );
+	p.appendChild( document.createTextNode("1 x 'Adeept' Raspberry Pi 2 kit available from Adeept's ebay store - ") );
+	var a = document.createElement("a");
+	a.href = "https://stores.ebay.com.au/Adeept?_trksid=p2047675.l2563";
+	a.appendChild( document.createTextNode("https://stores.ebay.com.au/Adeept?_trksid=p2047675.l2563") );
+	a.style.fontWeight = "bold";
+	p.appendChild(a);
+	p.appendChild( document.createTextNode(".") );
+	iotPan.appendChild(p);
+	
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("There are a number of kits available, ensure you purchase the 'RFID Starter Kit for Raspberry Pi 2 Model B/B+ Python with 40-Pin GPIO Board' as this is required for your assessments.") );
+	p.appendChild( document.createElement("br") );
+	p.appendChild( document.createTextNode("Order this as soon as possible so you are not waiting too long to get in to it.") );
+	iotPan.appendChild(p);
+	
+	var readme = document.getElementById(container);
+	readme.appendChild(iotFlip);
+	readme.appendChild(iotPan);
+}
+
+function itsm( container ) {
+	var itsmFlip = document.createElement("div");
+	itsmFlip.id = "itsmFlip";
+	itsmFlip.className = "flip";
+	var span = document.createElement("span");
+	span.appendChild( document.createTextNode("Textbook Order Details") );
+	span.style.fontWeight = "bold";
+	itsmFlip.appendChild(span);
+	span = document.createElement("span");
+	span.appendChild( document.createTextNode(" (show/hide)") );
+	itsmFlip.appendChild(span);
+	
+	var itsmPan = document.createElement("div");
+	itsmPan.id = "itsmPan";
+	itsmPan.className= "panel";
+	var p = document.createElement("p");
+	p.appendChild( document.createTextNode("For this qualification you will require continual access to the following text books.") );
+	itsmPan.appendChild(p);
+	var ul = document.createElement("ul");
+	var li = document.createElement("li");
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("'Research Methods for Business Students' by Mark Saunders, Philip Lewis and Adrian Thornhill, (5th Ed) - ISBN-13: 978-0273716860") );
+	li.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("The book is available to order in hard copy or as a downloadable e-book.") );
+	li.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("There are many sources for this book at varying prices, so choose your best source of supply for your requirement.") );
+	li.appendChild(p);
+	ul.appendChild(li);
+	itsmPan.appendChild(ul);
+	
+	var readme = document.getElementById(container);
+	readme.appendChild(itsmFlip);
+	readme.appendChild(itsmPan);
+}
+
+function itsus( container ) {
+	var itsusFlip = document.createElement("div");
+	itsusFlip.id = "itsusFlip";
+	itsusFlip.className = "flip";
+	var span = document.createElement("span");
+	span.appendChild( document.createTextNode("Textbook Order Details") );
+	span.style.fontWeight = "bold";
+	itsusFlip.appendChild(span);
+	span = document.createElement("span");
+	span.appendChild( document.createTextNode(" (show/hide)") );
+	itsusFlip.appendChild(span);
+	
+	var itsusPan = document.createElement("div");
+	itsusPan.id = "itsusPan";
+	itsusPan.className= "panel";
+	var p = document.createElement("p");
+	p.appendChild( document.createTextNode("For this qualification you will require continual access to the following text books.") );
+	itsusPan.appendChild(p);
+	var ul = document.createElement("ul");
+	var li = document.createElement("li");
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("'Greening Through IT - Information Technology for Environmental Sustainability' by Bill Tomlinson.") );
+	li.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("The book is available to order in hard copy or as a downloadable e-book.") );
+	li.appendChild(p);
+	p = document.createElement("p");
+	p.appendChild( document.createTextNode("Details are at: ") );
+	var a = document.createElement("a");
+	a.href = "http://mitpress-ebooks.mit.edu/product/greening-through";
+	a.appendChild( document.createTextNode("http://mitpress-ebooks.mit.edu/product/greening-through") );
+	a.style.fontWeight = "bold";
+	p.appendChild(a);
+	li.appendChild(p);
+	ul.appendChild(li);
+	itsusPan.appendChild(ul);
+	
+	var readme = document.getElementById(container);
+	readme.appendChild(itsusFlip);
+	readme.appendChild(itsusPan);
 }
 
 var i = document.createElement("style");
