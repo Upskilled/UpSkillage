e7425aee1a24551309e853701fb8d23786772c0d
Andrew Skeklios <Skeksalot@users.noreply.github.com>
2018-07-25 12:00:56 +0930

    Reworking the video popup

--- js_css_lib.js
+++ js_css_lib.js
@@ -37,7 +37,7 @@ function showCourse( course ) {
 // Creates an iframe as an in-browser popup
 // Requires each popup to be used must be paired with a div
 // The div's id must be the same as the 'givenName' argument in the function
-function popup( givenName, iframeSrc, iconSrc ) {
+function OLDpopup( givenName, iframeSrc, iconSrc ) {
 	var popupNum = 0;
 	var name = document.getElementById("popup0");
 	while( name != null ) {
@@ -71,8 +71,7 @@ function popup( givenName, iframeSrc, iconSrc ) {
 	var content = document.createElement("iframe");
 	content.classList = "modal-content";
 	content.id = "popupVid" + popupNum;
-	content.style = "width: 1280px; height: 720px;";
-	content.frameBorder = "0";
+	content.style = "width: 1280px; height: 720px; border:none;";
 	content.src = iframeSrc;
 	content.setAttribute('allowfullScreen', '');
 	content.setAttribute('mozallowfullScreen', '');
@@ -90,6 +89,74 @@ function popup( givenName, iframeSrc, iconSrc ) {
 		content.style.display = "none";
 		content.src = content.src;
 	}
+}
+
+// MK2 version of video popup, re-written for better loading
+// Removes the creation of the video content from when the page is loaded
+// to when the link is clicked
+// Creates an iframe as an in-browser popup
+function popup( givenName, iframeSrc, iconSrc ) {
+	// Produce a random id
+	var popupID = Math.floor( ( Math.random() * 1000 ) + 1 );
+	while ( document.getElementById( givenName.replace( /\s/g, '' ) + popupID ) != null ) {
+		popupID = Math.floor( ( Math.random() * 1000 ) + 1 );
+	}
+	
+	// Create the link and add to the container
+	var a = document.createElement("a");
+	var html = document.createTextNode(givenName);
+	a.id = "vidLink" + popupID;
+	a.className = "popupLink";
+	var icon = document.createElement("img");
+	icon.src = iconSrc;
+	icon.classList = "iconlarge activityicon";
+	icon.alt = givenName;
+	a.appendChild(icon);
+	a.appendChild(html);
+	document.getElementById(givenName).appendChild(a);
+	var content = document.createElement("iframe");
+	var exit = document.createElement("span");
+	var container = document.createElement("div");
+	
+	// Add dynamic actions (the popup bit)
+	a.onclick = function() {
+		// If first time clicking link, create content
+		if ( document.getElementById(givenName).children.length <= 1 ) {
+			// Create the container
+			container = document.createElement("div");
+			container.classList = "modal";
+			container.id = givenName.replace( /\s/g, '' ) + popupID;
+			// Create the close button for within the popup
+			exit = document.createElement("span");
+			exit.innerHTML = "&times;";
+			exit.classList = "close";
+			exit.id = "close" + popupID;
+			container.appendChild(exit);
+			// Create the iframe for within the popup
+			content = document.createElement("iframe");
+			content.classList = "modal-content";
+			content.id = "popupVid" + popupID;
+			content.style = "width: 1280px; height: 720px; border:none;";
+			content.src = iframeSrc;
+			content.setAttribute('allowfullScreen', '');
+			content.setAttribute('mozallowfullScreen', '');
+			content.setAttribute('webkitallowfullScreen', '');
+			container.appendChild(content);
+			document.getElementById(givenName).appendChild(container);
+
+			exit.onclick = function() {
+				// Hide the content
+				container.style.display = "none";
+				content.style.display = "none";
+				content.src = content.src;
+			}
+		}
+		
+		// Display the content
+		container.style.display = "block";
+		content.style.display = "block";
+	}
+	
 }
 
 // Set a new cookie value
