43259333abf755fb769893acaf8883876a71e954
Andrew Skeklios <Skeksalot@users.noreply.github.com>
2019-08-28 16:51:41 +0930

    Testing

--- MoodleRooms/MR_experimental.js
+++ MoodleRooms/MR_experimental.js
@@ -1328,6 +1328,7 @@ function insertReadmeMR() {
 
 function registerTocListeners() {
 	// Find each tile in table of contents
+	alert('Adding listeners');
 	var toc = document.getElementById('chapters');
 	var tiles = toc.getElementsByClassName('li');
 
