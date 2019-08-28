c04c069b118f8aa9f0ce2efbf610878308a02a24
Andrew Skeklios <Skeksalot@users.noreply.github.com>
2019-08-29 09:10:00 +0930

    tests

--- MoodleRooms/MR_experimental.js
+++ MoodleRooms/MR_experimental.js
@@ -1341,8 +1341,7 @@ function registerTocListeners() {
 // Using script to add onload funtion without wiping existing ones
 // https://stackoverflow.com/questions/807878/javascript-that-executes-after-page-load
 // Modified to only occur on course pages
-alert(window.location.pathname);
-if( window.location.pathname.startsWith('https://upskilled-sandbox.mrooms.net/course/view.php') || window.location.pathname.startsWith('http://upskilled-sandbox.mrooms.net/course/view.php') ) {
+if( window.location.pathname.startsWith('/course/view.php') || window.location.pathname.startsWith('/course/view.php') ) {
 	if( window.attachEvent ) {
 		window.attachEvent( 'onload', registerTocListeners );
 	} else {
